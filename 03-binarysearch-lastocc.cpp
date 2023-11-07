94f2d80ab9244b534b0cea458dd3c4e187dac7c0
RashMin <70662641+rashcasm@users.noreply.github.com>
2023-11-07 16:01:37 +0530

    Create 03-binarysearch-lastocc.cpp

---
+++ 03-binarysearch-lastocc.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int binarysearch(int a[], int n, int l, int r, int k)
+{   
+    int save = -1;
+    while (l<=r)
+    {
+        int mid = l + (r-l)/2;
+        if(a[mid]==k){
+//            return mid; 
+            save = mid;
+            l=mid+1;
+        }
+        else if (a[mid]>k){
+            r = mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    return save;
+}
+int main() {
+    int a[]={2,3,4,4,4,5,6,2,3,4};
+    int k = 4;
+    int n=sizeof(a)/sizeof(a[0]);
+    int index = binarysearch(a,n,0,n-1,k);
+    cout << "Last occurance of " << k << " is " << index << endl;
+    return 0;
+}
